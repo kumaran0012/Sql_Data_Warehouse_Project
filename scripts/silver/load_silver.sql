5c421f6b30e51427fa896861f3a33815d7c0ff05
kumaran0012 <143807650+kumaran0012@users.noreply.github.com>
2025-11-12 10:20:21 +0530

    Create load_silver.sql

---
+++ scripts/silver/load_silver.sql
@@ -0,0 +1,172 @@
+/*
+This Load_silver.sql will do a ETL(Extract,Transform,Load) from the bronze layer
+In this layer, The data is cleaned,Transformed and standarized,integrated and loaded
+*/
+CREATE OR REPLACE PROCEDURE silver.load_silver()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+	start_BATCH TIMESTAMP;
+    end_BATCH TIMESTAMP;
+    start_time TIMESTAMP;
+    end_time TIMESTAMP;
+begin
+	start_BATCH := NOW();
+	RAISE NOTICE 'LOADING SILVER LAYER';
+	start_time := NOW();
+	truncate table silver.crm_cust_info;
+	RAISE NOTICE 'Truncated table: silver.crm_cust_info';
+	insert into silver.crm_cust_info(
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+	)
+	select cst_id , cst_key, trim(cst_firstname) as cst_firstname , trim(cst_lastname) as cst_lastname , 
+	case 
+		when upper(trim(cst_material_status)) = 'M' then 'Married'
+		when upper(trim(cst_material_status)) = 'S' then 'Single'
+		else 'N/A' 
+	end cst_marital_status, 
+	case 
+		when upper(trim(cst_gndr)) = 'M' then 'Male'
+		when upper(trim(cst_gndr)) = 'F' then 'Female'
+		else 'N/A'
+	end cst_gndr, cst_create_date from
+	(select *,row_number() over (partition by cst_id order by cst_create_date desc) as dup from bronze.crm_cust_info )t 
+	where cst_id is not null and dup = 1;
+	RAISE NOTICE 'Loaded table: silver.crm_prd_info';
+	end_time := NOW();
+    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+	
+	start_time := NOW();
+	truncate table silver.crm_prd_info;
+	RAISE NOTICE 'Truncated table: silver.crm_prd_info';
+	insert into silver.crm_prd_info(prd_id,cat_id,prd_key,prd_nm,prd_cost,prd_line,prd_start_dt,prd_end_dt)
+	SELECT prd_id,
+	replace(substring(prd_key,0,6),'-','_') as cat_id,
+	substring(prd_key,7,length(prd_key)) as prd_key,
+	prd_nm,
+	COALESCE(prd_cost,0) as prd_cost,
+	case upper(trim(prd_line))
+		when 'M' then 'Mountain'
+		when 'S' then 'Other Sales' 
+		when 'R' then 'Road'
+		when 'T' then 'Touring'
+		else 'N/A' 
+		end as prd_line,
+	cast(prd_start_dt as date ) as prd_start_dt,
+	cast(lead(prd_start_dt) over (partition by prd_key order by prd_start_dt)-1 as date) as prd_end_dt from bronze.crm_prd_info;
+	RAISE NOTICE 'Loaded table: silver.crm_prd_info';
+	end_time := NOW();
+	RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+
+	start_time := NOW();
+	truncate table silver.crm_sales_details;
+	RAISE NOTICE 'Truncated table: silver.crm_sales_details';
+	insert into silver.crm_sales_details (
+	    sls_ord_num,
+	    sls_prd_key,
+	    sls_cust_id,
+	    sls_order_dt,
+	    sls_ship_dt,
+	    sls_due_dt,
+	    sls_sales,
+	    sls_quantity,
+	    sls_price 
+	)
+	select 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	case when sls_order_dt =0 or length(sls_order_dt::text) != 8 then null
+	else cast(cast(sls_order_dt as varchar) as date)
+	end as sls_order_dt,
+	case when sls_ship_dt =0 or length(sls_ship_dt::text) != 8 then null
+	else cast(cast(sls_ship_dt as varchar) as date)
+	end as sls_ship_dt,
+	case when sls_due_st =0 or length(sls_due_st::text) != 8 then null
+	else cast(cast(sls_due_st as varchar) as date)
+	end as sls_due_dt,
+	case when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * abs(sls_price)
+		then sls_quantity * abs(sls_price)
+		else sls_sales
+	end as sls_sales,
+	sls_quantity,
+	case when sls_price is null or sls_price <= 0
+	then sls_sales / nullif(sls_quantity,0)
+	else sls_price
+	end as sls_price
+	from bronze.crm_sales_details;
+	RAISE NOTICE 'Loaded table: silver.crm_sales_details';
+	end_time := NOW();
+	RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+
+	start_time := NOW();
+	truncate table silver.erp_cust_az12;
+	RAISE NOTICE 'Truncated table: silver.erp_cust_az12';
+	INSERT INTO silver.erp_cust_az12(
+	cid,
+	bdate,
+	gen
+	)
+	
+	select
+	case when cid like 'NAS%' then substring(cid, 4, length(cid))
+	else cid
+	end as cid,
+	case when bdate > current_date then null
+	else bdate
+	end as bdate,
+	case when upper(trim(gen)) in ('M','MALE') then 'Male'
+	when upper(trim(gen)) in ('F','FEMALE') then 'Female'
+	else 'N/A'
+	end as gen
+	from bronze.erp_cust_az12;
+	RAISE NOTICE 'Loaded table: silver.erp_cust_az12';
+	end_time := NOW();
+    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+
+	start_time := NOW();
+	truncate table silver.erp_loc_a101;
+	RAISE NOTICE 'Truncated table: silver.erp_loc_a101';
+	insert into silver.erp_loc_a101(
+	cid,
+	cntry
+	)
+	select 
+	replace(cid,'-','') cid,
+	case when trim(cntry) = 'DE' then 'Germany'
+	when trim(cntry) in ('US','USA') then 'United States'
+	when trim(cntry) = '' or cntry is null then 'N/A'
+	else trim(cntry)
+	end as cntry
+	from bronze.erp_loc_a101;
+	RAISE NOTICE 'Loaded table: silver.erp_loc_a101';
+	end_time := NOW();
+    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+
+	start_time := NOW();
+	truncate table silver.erp_px_cat_g1v2;
+	RAISE NOTICE 'Truncated table: silver.erp_px_cat_g1v2';
+	insert into silver.erp_px_cat_g1v2(
+	id,
+	cat,
+	subcat,
+	maintenance
+	)
+	select
+	id,
+	cat,
+	subcat,
+	maintenance
+	from bronze.erp_px_cat_g1v2;
+	RAISE NOTICE 'Loaded table: silver.erp_px_cat_g1v2';
+	end_time := NOW();
+    RAISE NOTICE 'Duration: % seconds', EXTRACT(EPOCH FROM (end_time - start_time));
+end $$;
+
+call silver.load_silver();
